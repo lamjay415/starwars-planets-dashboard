3eee5957938888cc9c8e28b07591621019746071
Jay Lam <lamjay415@gmail.com>
2021-11-12 14:44:50 -0800

    moved page buttons to table component

--- app/src/components/table.js
+++ app/src/components/table.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './table.css';
 
 const Table = ({planets}) =>{
-    const rows = planets.map((planet,idx) => {
+
+    const [pages] = useState([]);
+    const [page, setPage] = useState(0);
+
+    // let rows = [];
+
+    useEffect(() => {
+        for(let i = 0; i<planets.length;i+=10){
+            pages.push(planets.slice(i,i+10));
+        }
+
+    }, []);
+
+    const rows = pages[page].map((planet,idx) => {
 
         return(
             <div className='table-row' key={`row-${idx}`}>
@@ -15,20 +28,24 @@ const Table = ({planets}) =>{
                 <div>{planet.surface_water}</div>
             </div>
         )
-    })
+    });
 
     return (
-        <div className='planets-table'>
-            <div className='table-header'>
-                <div>Name</div>
-                <div>Population</div>
-                <div>Rotational Period</div>
-                <div>Orbital Period</div>
-                <div>Diameter</div>
-                <div>Climate</div>
-                <div>Surface Water</div>
+        <div className='table-container'>
+            <div className='planets-table'>
+                <div className='table-header'>
+                    <div>Name</div>
+                    <div>Population</div>
+                    <div>Rotational Period</div>
+                    <div>Orbital Period</div>
+                    <div>Diameter</div>
+                    <div>Climate</div>
+                    <div>Surface Water</div>
+                </div>
+                {rows}
             </div>
-            {rows}
+            <div onClick={()=>setPage(page>0 ? page - 1 : 0)}>Previous</div>
+            <div onClick={()=>setPage(page < planets.length - 1 ? page + 1 : page)}>Next</div>
         </div>
     )
 }
